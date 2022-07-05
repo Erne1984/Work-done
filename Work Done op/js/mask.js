3982e5bc54ff7b69d95275445f077ac39c32a5b0
Ernesto Floriano Amorim <99537255+Erne1984@users.noreply.github.com>
2022-07-05 18:39:31 -0300

    Add files via upload

---
+++ Work Done op/js/mask.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#cpf').mask('999.999.999-99')
+    $('#Email').mask('999999999')
+    $('#Senha').mask('99999-999')
+    $('#DataNascimento').mask('999999')
+})
